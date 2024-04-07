060a07f4cc3d404b24dad3d52fdc81eb5adc65cb
22FTT1503 <22FTT1503@student.pb.edu.bn>
2024-04-07 23:05:29 +0800

    Remove unused pages and routing modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,37 +4,42 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    loadChildren: () =>
+      import('./tabs/tabs.module').then((m) => m.TabsPageModule),
   },
-  {
-    path: 'roastedturkey',
-    loadChildren: () => import('./roastedturkey/roastedturkey.module').then( m => m.RoastedturkeyPageModule)
-  },
+
   {
     path: 'cranberry',
-    loadChildren: () => import('./cranberry/cranberry.module').then( m => m.CranberryPageModule)
+    loadChildren: () =>
+      import('./tab2/cranberry/cranberry.module').then(
+        (m) => m.CranberryPageModule
+      ),
   },
   {
     path: 'cookies',
-    loadChildren: () => import('./cookies/cookies.module').then( m => m.CookiesPageModule)
+    loadChildren: () =>
+      import('./tab2/cookies/cookies.module').then((m) => m.CookiesPageModule),
   },
   {
     path: 'beef',
-    loadChildren: () => import('./beef/beef.module').then( m => m.BeefPageModule)
+    loadChildren: () =>
+      import('./tab2/beef/beef.module').then((m) => m.BeefPageModule),
   },
   {
     path: 'ketupat',
-    loadChildren: () => import('./ketupat/ketupat.module').then( m => m.KetupatPageModule)
+    loadChildren: () =>
+      import('./tab2/ketupat/ketupat.module').then((m) => m.KetupatPageModule),
   },
   {
     path: 'wajid',
-    loadChildren: () => import('./wajid/wajid.module').then( m => m.WajidPageModule)
-  }
+    loadChildren: () =>
+      import('./tab2/wajid/wajid.module').then((m) => m.WajidPageModule),
+  },
 ];
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/tab2/roastedturkey/roastedturkey.page.ts
+++ src/app/tab2/roastedturkey/roastedturkey.page.ts
@@ -6,10 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./roastedturkey.page.scss'],
 })
 export class RoastedturkeyPage implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
